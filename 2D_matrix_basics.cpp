37dee474c3933dfbbe81f69271426053b05378d0
Esha Thakur <eshathakur888@gmail.com>
2022-10-19 00:51:46 +0530

    2d

---
+++ 2D_matrix_basics.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// linear search function
+void search2D(int arr[][4], int row, int col, int target)
+{
+    bool sum=0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (arr[i][j] == target)
+            {
+                cout<<"Element is Present at index: "<<i<<" "<<j<<endl;
+                sum=1;
+                break;
+            }
+        }
+    }
+    if(sum){
+        cout<<"Present verified"<<endl;
+    }
+    else{
+        cout<<"not present"<<endl;
+    }
+}
+
+// row wise sum
+void rowSum(int arr[][4], int row, int col)
+{
+    cout<<"ROW WISE SUM: ";
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << sum<<" ";
+    }
+}
+
+//column wise sum
+void colSum(int arr[][4], int row,int col){
+    cout<<"COLUMN WISE SUM: ";
+    for (int i = 0; i < col; i++)
+    {
+        int sum=0;
+        for (int j = 0; j < row; j++)
+        {
+            sum+=arr[j][i];
+        }
+        cout<<sum<<" ";
+    }
+    
+}
+
+int main()
+{
+    int arr[3][4] = {{2, 4, 5, 7},
+                     {1, 3, 4, 5},
+                     {6, 1, 8, 2}};
+
+    // taking input row-wise
+    //  for(int i=0; i<3;i++){
+    //      for (int j= 0; j < 4; j++)
+    //      {
+    //          cin>>arr[i][j];
+    //      }
+
+    // }
+
+    // taking input col-wise
+    //  for(int i = 0; i < 4; i++)
+    //  {
+    //      for (int j = 0; j < 3; j++)
+    //      {
+    //          cin>>arr[j][i];
+    //      }
+
+    // }
+
+    // print elements
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // linear search in 2D matrix
+    // if (search2D(arr, 3, 4, 6))
+    // {
+    //     cout << "Element is present."<<endl;
+    // }
+    // else
+    // {
+    //     cout << "Element is not present."<<endl;
+    // }
+
+    search2D(arr,3,4,11);
+    // row wise sum
+    rowSum(arr, 3, 4);
+    colSum(arr,3,4);
+    return 0;
+}
